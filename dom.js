89446fc0ef5457e7b30c5296a0da4adc64d0a4ad
DanielDdungu <daddungu@outlook.com>
2021-11-06 12:57:34 +0100

    Final Interface

--- dom.js
+++ dom.js
@@ -1,13 +1,18 @@
 var form = document.getElementById('addForm');
 var productList = document.getElementById('products');
+var filterProducts = document.getElementById('filter')
 
-//Form submit event
+//FORM SUBMIT EVENT
 form.addEventListener('submit', addProduct);
 
 //DELETE EVENT
 productList.addEventListener('click', removeProducts);
 
-//Add Product
+
+//ADD FILTER EVENT
+filterProducts.addEventListener('keyup', filterProduct);
+
+//ADD PRODUCT FUNCTION
 function addProduct(e) {
     e.preventDefault();
 
@@ -54,5 +59,33 @@ function removeProducts(e) {
             productList.removeChild(li);
         }
     }
+
+}
+
+
+
+//CREATE FILTER PRODUCT FUNCTION
+function filterProduct(e) {
+    //convert to lower case of every thing being typed in
+    var text = e.target.value.toLowerCase();
+
+    //grab all the product list items
+    var products = productList.getElementsByTagName('li');
+    //console.log(products);
+
+    //convert the HTML list into an array
+    Array.from(products).forEach(function(products) {
+        var productName = products.firstChild.textContent;
+        //console.log(productName);
+
+        //COMPARING WHAT IS BEING TYPED IN
+        if (productName.toLocaleLowerCase().indexOf(text) !=
+            -1) {
+            products.style.display = 'block';
+
+        } else {
+            products.style.display = 'none';
+        }
+    });
 
 }
